8361c0572ccb91890fb9183b0d0cb865c3a0c01f
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-06-02 11:02:50 -0400

    Add fenced code to test
    Also clean up tests.

--- tests/testthat/test-exrcise.R
+++ tests/testthat/test-exrcise.R
@@ -1,22 +1,25 @@
 test_that("Exrcise transforms correctly", {
   in_test <- "reference.Rmd"
+  out_test <- paste0(in_test, ".ex")
   blank_test <- "blanked.Rmd"
   rep_test <- "replaced.Rmd"
   comment_test <- "commented.Rmd"
 
-  expect_equal(exrcise(in_test, paste0(in_test, ".ex"),
+  expect_equal(exrcise(in_test, out_test,
                        replace_flags = "solution",
                        comment = FALSE),
                readr::read_lines(blank_test))
 
-  expect_equal(exrcise(in_test, paste0(in_test, ".ex"),
+  expect_equal(exrcise(in_test, out_test,
                        replace_flags = "solution",
                        comment = TRUE),
                readr::read_lines(comment_test))
 
-  expect_equal(exrcise(in_test, paste0(in_test, ".ex"),
+  expect_equal(exrcise(in_test, out_test,
                        replace_flags = "solution",
                        replacement = "### YOUR CODE HERE",
                        comment = FALSE),
                readr::read_lines(rep_test))
+  #clean up
+  file.remove(out_test)
 })
